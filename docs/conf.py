1b72851851c9aa6a6168ada258f404a51c1e39ee
Kristopher Cooper <kris@infra04-wg012.eduroam.wireless.umn.edu>
2025-10-02 13:59:15 -0500

    removed numpydoc

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,6 @@ release = '1.0.0'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'numpydoc',  # Needs to be loaded *after* autodoc.
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     'sphinx.ext.githubpages',
